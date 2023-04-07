70a165cd9c4bfcc50ae044e940e13b43e5603424
nokosse <visouthivong.kevin@gmail.com>
2023-04-07 14:52:39 +0200

    norms

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 17:28:02 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/04/07 14:44:06 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/04/07 14:52:15 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	main(int ac, char **av)
 {
-	(void)av;
 	if (!valid_args_check(ac, av) || !check_int_max(av))
 		return (write(2, "Error: Invalid arguments\n", 25), 0);
 	printf("Correct arguments\n");
