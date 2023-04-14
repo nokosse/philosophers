0fa010019102bdddcdae5ad92b4361bb14729f7d
nokosse <visouthivong.kevin@gmail.com>
2023-04-14 16:56:04 +0200

    added comments

--- philo/src/init_thread.c
+++ philo/src/init_thread.c
@@ -6,16 +6,19 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 15:47:52 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/04/14 16:47:38 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/04/14 16:54:16 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
 // This function will initialize the threads.
-// First, in the loop, we create a thread for each philosopher.
-// Then, we create a thread for the check_death function.
-// At the end, we join the thread_ids.
+// There will be 2 'type' of threads :
+// - Philosophers threads, which will be the philosophers themselves.
+// - Death thread, which will check if a philosopher is dead.
+//
+// Because the reason for a thread to exist, is to execute a function.
+// One thread will use 'routine', and the other will use 'check_death'.
 void	init_threads(t_arg *args)
 {
 	int			total_philos;
@@ -28,6 +31,7 @@ void	init_threads(t_arg *args)
 	{
 		pthread_create(&threads[total_philos], NULL, routine,
 			(void *)&args->philo_num[total_philos]);
+		total_philos--;
 	}
 	pthread_create(&thread_ids, NULL, check_death, (void *)args->philo_num);
 	pthread_join(thread_ids, NULL);
